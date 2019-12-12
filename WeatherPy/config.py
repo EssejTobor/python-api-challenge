7355f9d82c9d45cee34ab33efa2fb297e8bc2d3a
Jesse Kowalcic <jesseckowalcic@gmail.com>
2019-12-11 22:46:36 -0500

    Update config.py

--- WeatherPy/config.py
+++ WeatherPy/config.py
@@ -1,9 +1,2 @@
-# Add your API key
-NYTapi_key = "8vlHdKFB3B96AaomH2XcLLtVWpNeUeME"
+
 OWapi_key = "af897208984399fb49a229ad7cef72e5"
-
-#Use this key in your application by passing it with key=API_KEY parameter.
-g_key = "AIzaSyCkD0UYo8GFehFy3pmX8EOQ3rnxKuhSzDA"
-
-#Census Key
-cen_key = 'f2537d961421ffc236f28118d3d08551df50ca9f'
